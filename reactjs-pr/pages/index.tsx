8a0e9eeba70d3712e7e47ce5c36a56e3e3c57cdd
takayukifukuhara <takayuki@curama.jp>
2020-03-08 19:24:24 +0900

    :+1:linkの生成

--- reactjs-pr/pages/index.tsx
+++ reactjs-pr/pages/index.tsx
@@ -1,1 +1,12 @@
-export default () => <div>Hello next.js!</div>;
+import Link from "next/Link";
+
+const Index = () => (
+  <div>
+    <h1>Index Page</h1>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+  </div>
+);
+
+export default Index;
